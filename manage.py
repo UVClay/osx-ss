fb937619ba0fe90589686232fddacc6024fecb81
UVClay <clayallen3@gmail.com>
2015-11-20 11:26:27 -0500

    We're stable for now

--- manage.py
+++ manage.py
@@ -1,16 +1,16 @@
+#!/usr/local/bin/python3
 # OS X Screenshot Automation
 # github.com/uvclay/osx-ss
 # Alpha whatever it works so who cares
-# Requires: fswatch, terminal-notifier
+# Requires (homebrew): fswatch, terminal-notifier
+# Requires (pip3): daemonize
 
-import argparse
+# import argparse
 import configparser
-import os
 import random
 import string
 import subprocess
 from ftplib import FTP
-from glob import glob
 
 config = configparser.ConfigParser()
 config.read('ss.ini')
@@ -35,15 +35,17 @@ def main():
 
 
 def upload(line):
-    print(line)
-    exit()
-    new = gen()+'.png'
+    new = gen() + '.png'
+    print("made it to the upload function")
     ftp = FTP(config[_server]['FTPHost'], config[_server]['FTPUser'], config[_server]['FTPPass'])
-    img = open(file, "rb")
-    ftp.storbinary('STOR '+new, img)
-    url = config[_server]['WebRoot']+new
+    img = open(line, "rb")
+    print("made it to the img open statement")
+    ftp.storbinary('STOR ' + new, img)
+    url = config[_server]['WebRoot'] + new
     img.close()
-    subprocess.Popen(["terminal-notifier", "-title", "Screenshot Uploaded!", "-message", "Image uploaded to: " + url + "", "-open", url, "-sound", "default"])
+    subprocess.Popen(
+        ["terminal-notifier", "-title", "Screenshot Uploaded!", "-message", "Image uploaded to: " + url + "", "-open",
+         url, "-sound", "default"])
     ftp.quit()
 
 
@@ -53,23 +55,17 @@ def gen(size=5, chars=string.ascii_letters + string.digits):
 
 
 def exttest(line):
-    #full, ext = os.path.splitext(line[:-1])
-    line = line[:-1]
-    print("testing {0} for .png extension".format(line))
     if line.lower().endswith(b'.png'):
-        print(line.lower().endswith(b'.png'))
-        print("it worked!")
-        #upload(line[:-1])
-    else:
-        print(line.lower().endswith(b'.png'))
-        print("not .png")
+        upload(line)
 
 
 def execute(command):
     popen = subprocess.Popen(command, stdout=subprocess.PIPE)
     lines_iterator = iter(popen.stdout.readline, b"")
     for line in lines_iterator:
+        line = line[:-1]
         exttest(line)  # yield line
+
 
 if __name__ == "__main__":
     main()
