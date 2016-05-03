9ee72b10c5a1c5c53596347d3d09495613e7d1ea
mathew <backwards-rat-race@users.noreply.github.com>
2016-05-03 21:43:11 +0100

    Implemented Imgur API. Not very reliable currently

--- handlers/imgur.py
+++ handlers/imgur.py
@@ -1,2 +1,32 @@
-class imgur( object ):
-    pass
+from handlers.handler import handler
+import requests
+from base64 import b64encode
+from os import path
+
+class imgur(handler):
+    def __init__(self, file, server):
+        handler.__init__(self, file, server)
+        self.url = None
+        self.apiKey = None
+        self.clientId = None
+        self.url = None
+
+    def setConfig(self, val):
+        self.config = val
+        self.clientId = self.config[self.server]['ClientId']
+        self.apiKey = self.config[self.server]['ApiKey']
+        self.url = self.config[self.server]['Url']
+
+    def upload(self):
+        image = b64encode(open(self.file, 'rb').read())
+        name = path.split(self.file)[1]
+
+        imgur_req = requests.post(
+            self.url,
+            headers = {'Authorization' : 'Client-ID %s' % self.clientId},
+            data = {
+                'image': image
+            }
+        )
+        print(imgur_req.text)
+        return 'done'
